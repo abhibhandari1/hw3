9641ef5bd147cb86fd44a95f6bec804291c9e571
abhibhandari1 <abhishek.bhandari@kellogg.northwestern.edu>
2022-05-20 18:15:48 +0000

    -

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,7 +4,19 @@ class PlacesController < ApplicationController
     @places = Place.all
     end
 
-    
+    def show
+    end
+
+    def new
+        @places = Place.new
+    end
+
+    def create
+        @places = Place.new
+        @places["name"] = params["place"]["name"]
+        @places.save
+        redirect_to "/places"
+    end
 
 
 
