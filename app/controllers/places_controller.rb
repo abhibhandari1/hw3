8534b57d3d48a4df7b1dbf0bd78a0e803da0fe8f
abhibhandari1 <abhishek.bhandari@kellogg.northwestern.edu>
2022-05-21 01:16:44 +0000

    -

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,8 +5,8 @@ class PlacesController < ApplicationController
     end
 
     def show
-    @places = Place.find_by({ "id" => params["id"] })
-    @posts = Post.where({ "place_id" => @places["id"] })
+         @places = Place.find_by({ "id" => params["id"] })
+         @posts = Post.where({ "place_id" => @places["id"] })
     end
 
     def new
